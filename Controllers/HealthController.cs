f3d8708aee491fb6bdd62cd433bf747b0f700d98
Alvaro T <goodbytes23@gmail.com>
2020-01-24 11:11:16 +0100

    Added Test project and health controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace nomadas.network.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok("OK");
+        }
+    }
+}
