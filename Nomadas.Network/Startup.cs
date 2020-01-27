e5d35203a70581a42eb3c017a1caf8dc889b6fb4
Alvaro T <goodbytes23@gmail.com>
2020-01-27 12:44:56 +0100

    Fixed Sql string

--- Nomadas.Network/Startup.cs
+++ Nomadas.Network/Startup.cs
@@ -33,7 +33,7 @@ namespace Nomadas.Network
             var pass = Environment.GetEnvironmentVariable("DB_PASSWORD");
             var user = Environment.GetEnvironmentVariable("ADMIN_LOGIN");
             var dbname = Environment.GetEnvironmentVariable("DB_NAME");
-            return $"Server={server};Database={dbname};User={user};Password={pass};";
+            return $"Server=tcp:{server},1433;Initial Catalog={dbname};Persist Security Info=False;User ID={user};Password={pass};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
